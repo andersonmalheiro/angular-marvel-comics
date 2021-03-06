99b53473327cc917e2bd67ec64a9bb9937eadd96
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-03-06 16:22:07 -0300

    Updating app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { ComicsService } from './api/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'marvel-comics-angular';
+
+  constructor(private comicService: ComicsService) {}
+
+  ngOnInit(): void {
+    this.comicService
+      .list({ limit: 10 })
+      .pipe(take(1))
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApiModule } from './api/api.module';
+import { httpInterceptorProviders } from './api/interceptors';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ApiModule, HttpClientModule],
+  providers: [httpInterceptorProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
